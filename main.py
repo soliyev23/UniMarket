97e742762c282126b38118bc31a87f12d1216121
Abdumalik <asoliyev53@gmail.com>
2025-10-08 16:02:16 +0500

    Comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI 
-app = FastAPI(title="UniMarket", 
-version="0.1.0") 
+app = FastAPI(title="UniMarket", version="0.1.0") 
 @app.get("/health") 
 def health(): 
     return {"status": "ok"}
